e18d7f2579e284fb1a3b3e2f02ac0f9dd5292c56
Arshxll <70693770+Arshxll@users.noreply.github.com>
2020-10-24 14:55:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="em">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> SVG TEXT PATH </title>
+	<link rel="stylesheet" href="style.css">
+	
+</head>
+<body>
+
+<svg width="900" height="780" style="outline: 0px solid black; font-size: 9; overflow: visible;">  
+  <text x="1" y="30" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="60" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="90" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="120" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="150" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="180" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="210" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="240" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="270" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="300" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="330" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="360" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="390" >I can do all things through Christ who strengthens me.</text>  
+  <text x="1" y="420" >I can do all things through Christ who strengthens me.</text>  
+  <text x="50" y="450" > I can do all things through Christ who strengthens me.</text> 
+  <text x="70" y="480" > I can do all things through Christ who strengthens me.</text> 
+  <text x="90" y="510" > I can do all things through Christ who strengthens me.</text> 
+  <text x="120" y="540" > I can do all things through Christ who strengthens me.</text> 
+  <text x="150" y="570" > I can do all things through Christ who strengthens me.</text> 
+  <text x="180" y="600" > I can do all things through Christ who strengthens me.</text> 
+  <text x="200" y="630" > I can do all things through Christ who strengthens me.</text> 
+  <text x="230" y="660" > I can do all things through Christ who strengthens me.</text> 
+  <text x="250" y="680" > I can do all things through Christ who strengthens me.</text> 
+  <text x="280" y="700" > I can do all things through Christ who strengthens me.</text> 
+  <text x="620" y="700" > I can do all things through Christ who strengthens me.</text> 
+  <text x="300" y="710" > I can do all things through Christ who strengthens me.</text>
+  <text x="500" y="710" > I can do all things through Christ who strengthens me.</text>
+  <text x="700" y="710" > I can do all things through</text>
+  <text x="300" y="720" > I can do all things through Christ who strengthens me.</text>
+  <text x="500" y="720" > I can do all things through Christ who strengthens me.</text>
+  <text x="700" y="720" > I can do all things through</text>
+  <text x="300" y="730" > I can do all things through Christ who strengthens me.</text>
+  <text x="500" y="730" > I can do all things through Christ who strengthens me.</text>
+  <text x="700" y="730" > I can do all things through</text>
+  <text x="300" y="740" > I can do all things through Christ who strengthens me.</text>
+  <text x="500" y="740" > I can do all things through Christ who strengthens me.</text>
+  <text x="700" y="740" > I can do all things through</text>
+  <text x="300" y="750" > I can do all things through Christ who strengthens me.</text>
+  <text x="500" y="750" > I can do all things through Christ who strengthens me.</text>
+  <text x="700" y="750" > I can do all things through</text>
+  <text x="300" y="760" > I can do all things through Christ who strengthens me.</text>
+  <text x="500" y="760" > I can do all things through Christ who strengthens me.</text>
+  <text x="700" y="760" > I can do all things through</text>
+   <text x="300" y="770" > I can do all things through Christ who strengthens me.</text>
+  <text x="500" y="770" > I can do all things through Christ who strengthens me.</text>
+  <text x="700" y="770" > I can do all things through</text>
+   <text x="300" y="780" > I can do all things through Christ who strengthens me.</text>
+  <text x="500" y="780" > I can do all things through Christ who strengthens me.</text>
+  <text x="700" y="780" > I can do all things through</text>
+  
+  
+  
+  
+  <text x="650" y="680" > I can do all things through Christ who strengthens me.</text> 
+  <text x="680" y="660" > I can do all things through Christ who strengthens me.</text> 
+  <text x="710" y="630" > I can do all things through Christ who strengthens me.</text> 
+  <text x="730" y="600" > I can do all things through Christ who strengthens me.</text> 
+  <text x="760" y="570" > I can do all things through Christ who strengthens me.</text> 
+  <text x="790" y="540" > I can do all things through Christ who strengthens me.</text> 
+  <text x="820" y="510" > I can do all things through Christ who strengthens me.</text> 
+  <text x="850" y="480" > I can do all things through Christ who strengthens me.</text> 
+  <text x="880" y="450" > I can do all things through Christ who strengthens me.</text> 
+  <text x="910" y="420" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="390" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="360" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="330" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="300" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="270" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="240" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="210" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="180" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="150" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="120" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="90" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="60" > I can do all things through Christ who strengthens me.</text>
+  <text x="910" y="30" > I can do all things through Christ who strengthens me.</text>
+  <text x="200" y="30" > I can do all things through Christ who strengthens me.</text>
+  <text x="400" y="30" > I can do all things through Christ who strengthens me.</text>
+  <text x="600" y="30" > I can do all things through Christ who strengthens me.</text>
+  <text x="800" y="30" > I can do all things through </text>
+  <text x="1" y="10" > I can do all things through Christ who strengthens me.</text>
+  <text x="200" y="10" > I can do all things through Christ who strengthens me.</text>
+  <text x="400" y="10" > I can do all things through Christ who strengthens me.</text>
+  <text x="600" y="10" > I can do all things through Christ who strengthens me.</text>
+  <text x="800" y="10" > I can do all things through </text>
+  <text x="910" y="10" > I can do all things through Christ who strengthens me.</text>
+
+  
+</svg>
+
+
+
+</body>
+</html>
